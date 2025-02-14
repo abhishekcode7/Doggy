7e80857beb29e5c2aefec4538c06ad50989fe223
abhishek <chauhanabhi.mail@gmail.com>
2025-02-14 10:41:46 +0530

    Implemented Main Activity

--- app/src/main/java/com/example/doggy/MainActivity.kt
+++ app/src/main/java/com/example/doggy/MainActivity.kt
@@ -1,20 +1,21 @@
 package com.example.doggy
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.doggy.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding:ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.generateDogs.setBackgroundColor(Color.rgb(66,134,244))
+        binding.recentDogs.setBackgroundColor(Color.rgb(66,134,244))
     }
 }
